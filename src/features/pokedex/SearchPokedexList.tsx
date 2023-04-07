ae80fe0abde54383cb2a301511ff91a31e148a6c
Russell Terrell <russ134>
2023-04-07 14:08:34 -0400

    iteration on displaying Pokemon move contetn

--- src/features/pokedex/SearchPokedexList.tsx
+++ src/features/pokedex/SearchPokedexList.tsx
@@ -5,12 +5,20 @@ import { Pokemon } from './types/pokemontypes';
 import styles from './Pokedex.module.css';
 import axios from 'axios';
 
-
-
 const SearchPokemonList = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [pokemonMoves, setPokemonMoves] = useState<any[]>([]);
   const pokemonList = useSelector((state: RootState) => state.pokemon.pokemonList);
+
+  const [showPopup, setShowPopup] = useState(false);
+
+  function handleDivClick() {
+    setShowPopup(true);
+  }
+
+  function handleCloseClick() {
+    setShowPopup(false);
+  }
 
   const filteredPokemon = pokemonList.filter((pokemon: Pokemon) =>
     pokemon.name.includes(searchQuery.toLowerCase())
@@ -40,28 +48,26 @@ const SearchPokemonList = () => {
       <div id="searchresults" className={styles.row}>
         <div className={styles.resultwrapper}>
           {filteredPokemon.map((pokemon: Pokemon, index: number) => (
-            <div id="pokeitem" key={pokemon.id} className={styles.resultitem}>
+            <div id="pokeitem" key={pokemon.id} className={styles.resultitem} onClick={handleDivClick}>
               <img src={pokemon.imageUrl} alt={pokemon.name} />
               <p>{pokemon.name.charAt(0).toUpperCase()+pokemon.name.slice(1)}</p>
-              <div className={styles.container}>
-                <div className={styles.hoverable}>More</div>
-                <div className={styles.detailspopup}>
+              {showPopup && (
+                <div>
                   <h3>{pokemon.name.charAt(0).toUpperCase()+pokemon.name.slice(1)} Moves:</h3>
                   <ul>
                     {pokemonMoves[index]?.map((move: any, i: number) => (
-                      <li key={i}>{move.move.name}</li>
+                      <li key={i}>{move.move.name.charAt(0).toUpperCase()+move.move.name.slice(1)}</li>
                     ))}
                   </ul>
+                  <button onClick={handleCloseClick}>Close</button>
                 </div>
-              </div>
+              )}
             </div>
           ))}
         </div>
       </div>
     </div>
   );
-  };
-  
-   
+};
 
 export default SearchPokemonList;
